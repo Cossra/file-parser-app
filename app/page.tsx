446cc134d6d5eb9ec86d2d63210cce209dcb2edc
Cossra <cossrca@Gmail.com>
2025-08-07 11:17:57 -0400

    feat: add Header component with logo and home link

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-
-
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
-      <div className="bg-blue-500 text-white p-4 rounded">
-  Tailwind is working!
-</div>
     </div>
     
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+"use client";
+
+import { Shield } from "lucide-react";
+import Link from "next/Link";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="p-4 flex justify-between items-center">
+      <Link href="/" className="flex items-center">
+      <Shield className="w-6 h-6 text-blue-600 mr-2"/>
+      <h1 className="text-xl font-semibold">Expensio</h1>
+      </Link>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
